f43881ab44a017104e0dd8df0252f0a56e66f7b6
Thalles <thalles.lossurdo@invenit.com.br>
2024-05-02 18:30:15 -0300

    chore: use csv-parse to pipe the readable stream

--- index.mjs
+++ index.mjs
@@ -1,20 +1,22 @@
 import { createReadStream } from "fs";
-import csvParser from "csv-parser";
-const { parser } = csvParser;
+import { parse } from "csv-parse";
 
 const records = [];
 
-const stream = createReadStream("kepler_data.csv");
-
-stream.on("data", (chunk) => {
-  records.push(chunk);
-});
-
-stream.on("error", (err) => {
-  console.error(err);
-});
-
-stream.on("end", () => {
-  console.info("There will be no more data.");
-  console.log(records);
-});
+createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      columns: true,
+      comment: "#",
+    })
+  )
+  .on("data", (chunk) => {
+    records.push(chunk);
+  })
+  .on("error", (err) => {
+    console.error(err);
+  })
+  .on("end", () => {
+    console.info("There will be no more data.");
+    console.log(records);
+  });
